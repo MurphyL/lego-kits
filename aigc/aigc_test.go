bce72120e56cc317edd70ccb1d75c4371b7376d8
murph <murphyl@outlook.com>
2025-10-18 01:17:45 +0800

    使用 Option 模式重构 AIGC 模块改造

--- aigc/aigc_test.go
+++ aigc/aigc_test.go
@@ -2,23 +2,19 @@ package aigc
 
 import (
 	"encoding/json"
-	"net/http"
 	"os"
 	"testing"
 )
 
 func TestOllama(t *testing.T) {
-	agent := NewAgent(
-		ServiceProvider("http://localhost:11434/api/chat", "qwen3:8b"),
-	)
+	agent := NewAgent("http://localhost:11434/api/chat", "qwen3:8b")
 	functionsDictData, err := os.ReadFile("./test_functions.json")
 	if nil != err {
 		t.Error("读取自定义函数出错：", err.Error())
 	}
 	var functions []map[string]any
 	json.Unmarshal(functionsDictData, &functions)
-	messages := []CompletionMessage{{Role: MessageRoleUser, Content: "把武汉的天气发送到企微群里"}}
-	resp, err := agent.ApplyChatCompletion("qwen3:8b", messages)
+	resp, err := agent.ApplyChatCompletion("qwen3:8b", WithUserMessage("把武汉的天气发送到企微群里"))
 	if nil != err {
 		t.Error("调用 Ollama 失败", err.Error())
 	} else {
@@ -28,17 +24,14 @@ func TestOllama(t *testing.T) {
 }
 
 func TestOllamaV1(t *testing.T) {
-	agent := NewAgent(
-		ServiceProvider("http://localhost:11434/api/chat", "qwen3:8b"),
-	)
+	agent := NewAgent("http://localhost:11434/api/chat", "qwen3:8b")
 	functionsDictData, err := os.ReadFile("./test_functions.json")
 	if nil != err {
 		t.Error("读取自定义函数出错：", err.Error())
 	}
 	var functions []map[string]any
 	json.Unmarshal(functionsDictData, &functions)
-	messages := []CompletionMessage{{Role: MessageRoleUser, Content: "整理2025年内的假日和补班日，以如下JSON结构返回数据：[{\"dt\":\"日期\", \"kind\":\"补板或休假\", \"reason\":\"假日名称或补办原因\"}]"}}
-	resp, err := agent.ApplyChatCompletion("qwen3:8b", messages)
+	resp, err := agent.ApplyChatCompletion("qwen3:8b", WithUserMessage("整理2025年内的假日和补班日，以如下JSON结构返回数据：[{\"dt\":\"日期\", \"kind\":\"补板或休假\", \"reason\":\"假日名称或补办原因\"}]"))
 	if nil != err {
 		t.Error("调用 Ollama 失败", err.Error())
 	} else {
@@ -49,23 +42,18 @@ func TestOllamaV1(t *testing.T) {
 
 func TestXfyunX1(t *testing.T) {
 	token, ok := os.LookupEnv("XFYUN_AI_X1_TOKEN")
-	if !ok {
+	if ok {
+		agent := NewAgent("https://spark-api-open.xf-yun.com/v2/chat/completions", token)
+		resp, err := agent.ApplyChatCompletion("x1", WithUserMessage("武汉今天的天气怎么样？"))
+		if nil != err {
+			t.Error("调用 Xfyun AI 失败", err.Error())
+		} else {
+			bytes, _ := json.Marshal(resp)
+			t.Log(string(bytes))
+		}
+	} else {
 		t.Error("未配置讯飞云 X1 的 Token")
 		return
-	}
-	agent := NewAgent(
-		ServiceProvider("https://spark-api-open.xf-yun.com/v2/chat/completions", token),
-		CompletionRequestHook(func(request *http.Request) {
-			request.Header.Add("Content-Type", "application/json")
-		}),
-	)
-	messages := []CompletionMessage{{Role: MessageRoleUser, Content: "武汉今天的天气怎么样？"}}
-	resp, err := agent.ApplyChatCompletion("x1", messages)
-	if nil != err {
-		t.Error("调用 Xfyun AI 失败", err.Error())
-	} else {
-		bytes, _ := json.Marshal(resp)
-		t.Log(string(bytes))
 	}
 }
 
