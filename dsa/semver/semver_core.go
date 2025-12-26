ede5fba4687eebc0a933fb84e68ddf399c99add5
luohao <luohao@56lcy.com>
2025-12-26 14:15:32 +0800

    语义化版本

--- dsa/semver/semver_core.go
+++ dsa/semver/semver_core.go
@@ -1,22 +1,69 @@
 package semver
 
 import (
-	"log"
 	"regexp"
+	"strconv"
+	"strings"
 )
 
 // 参考文档 - https://semver.org/
-var SV_PATTERN = regexp.MustCompile("^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)$")
+var SV_PATTERN = regexp.MustCompile("(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(-[0-9a-zA-Z-]+)*")
 
-func Resolve(plainText string) SemVer {
-	matches := SV_PATTERN.FindAllString(plainText, -1)
-	log.Println(matches)
-	return nil
+func Resolve(plainText string) (SemVer, bool) {
+	matche := SV_PATTERN.FindString(plainText)
+	if len(matche) == 0 {
+		return nil, false
+	} else {
+		sv := internalSerVer{}
+		sv.versionStr, sv.additional, _ = strings.Cut(matche, "-")
+		parts := strings.SplitN(sv.versionStr, ".", 3)
+		sv.majorVersionNum, _ = strconv.ParseUint(parts[0], 10, 8)
+		sv.minorVersionNum, _ = strconv.ParseUint(parts[1], 10, 8)
+		sv.patchVersionNum, _ = strconv.ParseUint(parts[2], 10, 8)
+		return &sv, true
+	}
 }
 
 type SemVer interface {
-	Major() uint8
-	Minor() uint8
-	Patch() uint8
-	Labels() []string
+	Major() uint                    // 主版本号
+	Minor() uint                    // 次版本号
+	Patch() uint                    // 修正版本
+	Labels() []string               // 额外的标签：alpha, beta, snapshot
+	Value() string                  // 干净的版本号
+	Compare(otherSemVer SemVer) int // 对比两个版本
+}
+
+type internalSerVer struct {
+	versionStr      string
+	additional      string
+	majorVersionNum uint64
+	minorVersionNum uint64
+	patchVersionNum uint64
+}
+
+func (v *internalSerVer) Compare(otherSemVer SemVer) int {
+	return strings.Compare(v.versionStr, otherSemVer.Value())
+}
+
+func (v *internalSerVer) Major() uint {
+	return uint(v.majorVersionNum)
+}
+
+func (v *internalSerVer) Minor() uint {
+	return uint(v.minorVersionNum)
+}
+
+func (v *internalSerVer) Patch() uint {
+	return uint(v.patchVersionNum)
+}
+
+func (v *internalSerVer) Value() string {
+	return v.versionStr
+}
+
+func (v *internalSerVer) Labels() []string {
+	if len(v.additional) > 0 {
+		return strings.Split(v.additional, "-")
+	}
+	return []string{}
 }
