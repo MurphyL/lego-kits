f14ec6c65a18fbd3aefaa2503962b65b5c082c95
murph <murphyl@outlook.com>
2025-12-27 13:47:51 +0800

    开放平台基础类型

---
+++ open/open_platform.go
@@ -0,0 +1,15 @@
+package main
+
+import "net/http"
+
+func NewPlatformApp(key, secret string) *PlatformApp {
+	return &PlatformApp{key, secret}
+}
+
+type PlatformApp struct {
+	Key, Secret string // 开放平台基础信息
+}
+
+func (a *PlatformApp) ApplyEndpoint(r *http.Request) (*http.Response, error) {
+	return http.DefaultClient.Do(r)
+}
