a64220767790200b00c56d868d8be75636fca8dd
murph <murphyl@outlook.com>
2025-12-28 15:45:44 +0800

    通过 IP 获取当前位置的接口

--- open/lbs_outlet.go
+++ open/lbs_outlet.go
@@ -4,6 +4,10 @@ import (
 	"github.com/MurphyL/lego-kits/open/internal/location"
 )
 
-func GetPublicLocation() (*location.IPLocation, bool) {
-	return location.GetPublicLocation()
+func GetPublicLocation() PlatformResult[*location.IPLocation] {
+	if ret, ok := location.GetPublicLocation(); ok {
+		return NewResultWithCode(0, "操作成功", ret)
+	} else {
+		return NewResultWithCode[*location.IPLocation](1, "未知错误", nil)
+	}
 }
