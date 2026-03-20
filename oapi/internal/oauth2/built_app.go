9875b3145e74c16862a1b6b9f65d52d300fe6f54
luohao <luohao@56lcy.com>
2026-03-20 13:32:04 +0800

    feat(oauth2/wecom): 添加OAuth2基础接口和企业微信API代理结构
    添加OAuth2基础接口定义和企业微信API代理相关结构，包括WebhookAgent接口和实现

---
+++ oapi/internal/oauth2/built_app.go
@@ -0,0 +1,12 @@
+package oauth2
+
+type BuiltApp interface {
+}
+
+// 服务提供商（Service Provider）
+type ServiceProvider interface {
+}
+
+// ResourceOwner - 为资源所有者，用于表示用户在服务提供商中的资源所有者身份
+type ResourceOwner interface {
+}
--- oapi/internal/wecom/wework_server_api.go
+++ oapi/internal/wecom/wework_server_api.go
@@ -1,16 +1,18 @@
 package wecom
 
-import "fmt"
-
 const (
 	// 企业API基础URL
-	qyapiBaseEndpoint = "https://qyapi.weixin.qq.com/cgi-bin"
+	qyapiBaseURL = "https://qyapi.weixin.qq.com/cgi-bin"
 )
 
-type WebhookAgent struct {
-	url string
+func NewMessagePushAgent(key string) WebhookAgent {
+	return &QyapiWebhookAgent{baseURL: qyapiBaseURL}
 }
 
-func NewMessagePushAgent(key string) WebhookAgent {
-	return WebhookAgent{url: fmt.Sprintf("%s/webhook/send?key=%s", qyapiBaseEndpoint, key)}
+type WebhookAgent interface {
+}
+
+// QyapiWebhookAgent 企业API钩子代理
+type QyapiWebhookAgent struct {
+	baseURL string
 }
